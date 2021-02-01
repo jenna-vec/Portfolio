6e4ab6b55046998e4f7a44b4027b487dc94565e2
Jenna LeFort <71297454+jenna-vec@users.noreply.github.com>
2021-01-31 20:22:53 -0500

    event listener

--- index.js
+++ index.js
@@ -1,20 +1,5 @@
-//header
-let grid = document.getElementsByClassName("grid");
-let blueNoodle = document.getElementsByClassName("blue-noodle");
-let pills = document.getElementsByClassName("pills");
-let blueButton = document.getElementsByClassName("blue-button");
-let greenCircle = document.getElementsByClassName("green-circle");
-let pinkCone = document.getElementsByClassName("pink-cone");
-let greenButton = document.getElementsByClassName("green-button");
-let blueCircle = document.getElementsByClassName("blue-circle");
-let greenNoodle = document.getElementsByClassName("green-noodle");
-let pinkButton = document.getElementsByClassName("pink-button");
-let blueCone = document.getElementsByClassName("blue-cone");
-let bigText = document.getElementsByClassName("main-head-text");
-
-/*
 window.addEventListener("scroll", function(){
-    var yAxisValue = window.scrollY;
-    grid.style.top = yAxisValue + 'px';
-})
-*/
+    var offset = window.pageYOffset;
+    document.getElementById("shape").style.transform = "rotate(-1." + offset + "deg)";
+    document.getElementById("shape").style.top = -offset*0.9 + 'px';
+});
