a5625d95fabb1dec651341a9ea454d9b842f5530
Jenna LeFort <71297454+jenna-vec@users.noreply.github.com>
2021-02-01 21:38:41 -0500

    remove rotate

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 window.addEventListener("scroll", function(){
     var offset = window.pageYOffset;
-    document.getElementById("shape").style.transform = "rotate(-1." + offset + "deg)";
     document.getElementById("shape").style.top = -offset*0.9 + 'px';
 });
